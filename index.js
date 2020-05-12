cc9828600b65595df1f8b6eb66aa667bc4886a57
senniladri35 <senniladri35@gmail.com>
2020-05-13 02:32:05 +0530

    Async callbacks

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 var rect=require('./rectangle.js');
 
-function solverect(l,b)
-{
-    console.log("length :"+l + "braedth: " +b);
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
+};
 
-    if(l<=0 && b<=0)
-    {
-        console.log("length and breadth smmall");
-    }
-    console.log("area is"+rect.area(l,b));
-    console.log("perimeter is"+rect.perimeter(l,b));
-}
 
-
-solverect(2,3);
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);
